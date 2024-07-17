e83dafb2dbcf0cba411310303bdd2c04776a6f8f
Gowthamdevadiga <gowthamdevadiga@yahoo.com>
2024-07-17 10:47:02 +0530

    Updated script.js to ensure only numbers are entered in the input field

--- script.js
+++ script.js
@@ -1,32 +1,38 @@
-// var num=document.getElementById("number");
 
-// function getNum(){
-//     var comNum=Math.floor(Math.random()*10);
-//     console.log(comNum);
-//     if(num===comNum){
-//         document.getElementById("p").innerHTML="Your Guess is Correct";
-//     }else{
-//         document.getElementById("p").innerHTML="Your Guess is Wrong";
-//     }
-// }
 
-var num = document.getElementById("number");
-var resultText = document.getElementById("p");
-function getNum() {
-    var userNum = parseInt(num.value); // Get the value from the input and convert it to an integer
-    var comNum = Math.floor(Math.random() * 10) + 1; // Generate a random number between 1 and 10
-    console.log(comNum);
+document.addEventListener('DOMContentLoaded', function() {
+    var num = document.getElementById("number");
+    var resultText = document.getElementById("p");
 
-    if (userNum == comNum) {
-        resultText.innerHTML = "Your Guess is Correct";
-    } else {
-        resultText.innerHTML = "Your Guess is Wrong. The correct number was " + comNum;
+    function getNum() {
+        var userNum = parseInt(num.value); 
+        if (isNaN(userNum) || userNum>10||userNum<0) {
+            alert("Please enter a valid number between 1 and 10");
+            return;
+        }
+
+        var comNum = Math.floor(Math.random() * 10) + 1; 
+        console.log(comNum);
+        if (userNum === comNum) {
+            resultText.innerHTML = "Your Guess is Correct";
+        } else {
+            resultText.innerHTML = "Your Guess is Wrong. The correct number was " + comNum;
+        }
+        resultText.style.display = "block"; 
+        num.value = ""; 
     }
-    resultText.style.display = "block";
-    num.value=""; // Display the result
-}
 
-num.addEventListener('focus', function(){
-    resultText.style.display="none";
-})
+    num.addEventListener('focus', function() {
+        resultText.style.display = "none";
+    });
+
+    num.addEventListener('input', function() {
+        if (isNaN(num.value)) {
+            alert("Please enter only numbers");
+            num.value = "";
+        }
+    });
+
+    document.querySelector('button').addEventListener('click', getNum);
+});
 
